ffb7900cd89acdbb987261d00ccc743e7ca15c3a
CristopherMartarello <crisrossi1313@gmail.com>
2025-04-08 15:41:30 -0300

    feat: add size and default variant to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,11 @@
-const Button = ({ text, icon, variant = 'primary', ...rest }) => {
+const Button = ({
+  text,
+  icon,
+  variant = 'primary',
+  size = 'small',
+  className,
+  ...rest
+}) => {
   const getVariantColors = () => {
     if (variant === 'primary') {
       return 'bg-[#00AD85] text-white';
@@ -7,11 +14,25 @@ const Button = ({ text, icon, variant = 'primary', ...rest }) => {
     if (variant === 'secondary') {
       return 'bg-transaparent text-[#818181]';
     }
+
+    if (variant === 'default') {
+      return 'bg-[#EEEEEE] text-[#35383E]';
+    }
+  };
+
+  const getSizeClasses = () => {
+    if (size === 'small') {
+      return 'px-3 py-2 text-xs';
+    }
+
+    if (size === 'large') {
+      return 'py-3 px-4 text-sm';
+    }
   };
 
   return (
     <button
-      className={`flex items-center gap-1 rounded-md px-3 py-2 text-xs ${getVariantColors()} transition hover:opacity-80`}
+      className={`flex items-center justify-center gap-1 rounded-md ${getSizeClasses()} ${getVariantColors()} transition hover:opacity-80 ${className}`}
       {...rest}
     >
       {text}
