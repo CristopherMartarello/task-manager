95d4bdc1f03429a1c32e65b85f66f2ae38002177
CristopherMartarello <crisrossi1313@gmail.com>
2025-04-08 19:15:42 -0300

    feat: ensure fields are correctly filled and required on save task

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -24,6 +24,10 @@ const AddTaskDialog = ({ isOpen, handleClose, handleSubmit }) => {
   }, [isOpen]);
 
   const handleSaveClick = () => {
+    if (!title.trim() || !time.trim() || !description.trim()) {
+      return alert('Preencha todos os campos.');
+    }
+
     handleSubmit({
       id: v4(),
       title,
