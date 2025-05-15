41e6d4179016564d6ce351c24730fb6039d92ae4
CristopherMartarello <crisrossi1313@gmail.com>
2025-05-15 16:38:55 -0300

    feat: add Home Page

---
+++ src/components/DashboardCards.jsx
@@ -0,0 +1,47 @@
+import { LoaderIcon, Tasks2Icon, TasksIcon } from '../assets/icons';
+import { useGetTasks } from '../hooks/data/use-get-tasks';
+import DashboardCard from './DashboardCard';
+
+const DashboardCards = () => {
+  const { data: tasks } = useGetTasks();
+
+  const notStartedTasks = tasks?.filter(
+    (task) => task.status === 'not_started'
+  ).length;
+  const inProgressTasks = tasks?.filter(
+    (task) => task.status === 'in_progress'
+  ).length;
+  const doneTasks = tasks?.filter((task) => task.status === 'done').length;
+
+  return (
+    <div className="grid grid-cols-4 gap-9">
+      <DashboardCard
+        icon={<Tasks2Icon />}
+        mainText={tasks?.length}
+        secondaryText="Tarefas totais"
+      />
+      <DashboardCard
+        icon={<LoaderIcon />}
+        mainText={notStartedTasks}
+        secondaryText="Tarefas não iniciadas"
+      />
+      <DashboardCard
+        icon={<LoaderIcon />}
+        mainText={inProgressTasks}
+        secondaryText="Tarefas em andamento"
+      />
+      <DashboardCard
+        icon={<TasksIcon />}
+        mainText={doneTasks}
+        secondaryText="Tarefas em concluídas"
+      />
+      {/* <DashboardCard
+        icon={<GlassWaterIcon />}
+        mainText={5}
+        secondaryText="Água"
+      /> */}
+    </div>
+  );
+};
+
+export default DashboardCards;
